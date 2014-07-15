5859167a7281fa44ecddc54f24a326121bf7ee07
sasensio <sasensio>
2014-07-15 19:00:08 +0200

    SPI library for microchip 16f88
    SPI library for microchip 16f88
    Not tested

---
+++ spi.c
@@ -0,0 +1,34 @@
+/*spi.c
+
+
+*/
+
+
+void spi_init()
+{
+//PIN config	
+	TRISB1=1;
+	TRISB2=0;
+	TRISB4=0;
+	
+	sspstat=0x40;   /*0100 0000
+					7 SMP: 	0  	Input data sampled at middle of data output time
+					6 CKE: 	1	Transmit occurs on transition from Idle to active clock state
+					5 D/A: 	0	only for I2C
+					4 P:	0	only for I2C
+					3 S:	0	only for I2C
+					2 R/W:	0	only for I2C
+					1 UA:	0	only for I2C
+					0 BF:	0	buffer state
+					*/
+					
+	sspcon=	0x30;		
+	
+					/*xxx1 
+					7 WCOL: 	0 		No collision
+					6 SSPOV		0 		No overflow
+					5 SSPEN		0		ports as serial port configured
+					4 CKP		1  		clk idle is high		
+					3-0 SSPM: 	0000 	SPI Master mode, clock = OSC/4
+					*/
+}
---
+++ spi.h
@@ -0,0 +1,14 @@
+/*spi.h
+
+
+SDO: 	RB2
+SDI:	RB1
+SCK:	RB4
+
+*/
+
+
+
+void spi_init();
+
+
